dcee4c3d97c7348bdba6bb023462475e295d2f81
thanhdinhvan <dinhvanthanh2510@gmail.com>
2021-11-13 23:15:05 +0700

    vanthanh

---
+++ app/src/main/java/com/example/docbaoapp/view/GioiThieuActivity.java
@@ -0,0 +1,16 @@
+package com.example.docbaoapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.docbaoapp.R;
+
+public class GioiThieuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gioi_thieu);
+    }
+}
